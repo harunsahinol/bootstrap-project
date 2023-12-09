046c64d790c8fb80890e8e07d7590bc7c3015f38
harunsahinol <mehmethsh@gmail.com>
2023-12-09 15:26:01 +0300

    navbar problem fixed

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-//Navigation kısmında tıklanılan sayfaya active class ekleme
+const activePage = window.location.pathname;
 
-const navLinks = document.querySelectorAll('.nav-link');
-
-navLinks.forEach((navLink) => {
-  navLink.addEventListener('click', () => {
-    document.querySelector('.active')?.classList.remove('active');
-    navLink.classList.add('active');
-  });
+const navLinks = document.querySelectorAll(".nav-link").forEach((link) => {
+  if(link.href.includes(`${activePage}`)) {
+    link.classList.add("active");
+  }
+    
 });
