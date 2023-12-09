d1f0ac8fe7ebc17c013be9e26172b5c16bf1ba15
harunsahinol <mehmethsh@gmail.com>
2023-12-09 11:51:21 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+//Navigation kısmında tıklanılan sayfaya active class ekleme
+
+const navLinks = document.querySelectorAll('.nav-link');
+
+navLinks.forEach((navLink) => {
+  navLink.addEventListener('click', () => {
+    document.querySelector('.active')?.classList.remove('active');
+    navLink.classList.add('active');
+  });
+});
